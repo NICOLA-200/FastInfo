ba6fab63a843b613ce60dedb868717bab5b8de8b
NICOLA-200 <122203222+NICOLA-200@users.noreply.github.com>
2024-04-10 09:31:07 +0200

    Add files via upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
+import { Route ,Routes ,BrowserRouter } from "react-router-dom"
 import './App.css'
-import { Dashboard } from './pages'
+import { Dashboard, Login ,Process,Signup , Startup } from './pages'
 
 const App = () => {
 
   return (
-    <>
-      <Dashboard />
-    </>
+    <BrowserRouter>
+       <Routes>
+         <Route path="/" index Component={Dashboard} />
+         <Route path="/Login"  Component={Login} />
+         <Route path="/Signup"  Component={Signup} />
+         <Route path="/Startup" Component={Startup} />
+         <Route path="/registrationProcess" Component={Process} />
+          
+      </Routes>
+    
+    </BrowserRouter>
   )
 }
 
