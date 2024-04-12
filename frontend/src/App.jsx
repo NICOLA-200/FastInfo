bebd1e5b69306d299570f0fd5b5cf6162bc4e7b7
NICOLA-200 <122203222+NICOLA-200@users.noreply.github.com>
2024-04-13 01:52:09 +0200

    Add files via upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { Route ,Routes ,BrowserRouter } from "react-router-dom"
 import './App.css'
-import { Dashboard, Login ,Process,Signup , Startup } from './pages'
+import { Dashboard, Login ,Process,Signup , Startup , Profile, SearchPage } from './pages'
 
 const App = () => {
 
@@ -12,11 +12,13 @@ const App = () => {
          <Route path="/Signup"  Component={Signup} />
          <Route path="/Startup" Component={Startup} />
          <Route path="/registrationProcess" Component={Process} />
+         <Route path="/Profile" Component={Profile} />
+         <Route path="/SearchPage" Component={SearchPage} />
           
       </Routes>
     
     </BrowserRouter>
   )
 }
-
+  
 export default App
